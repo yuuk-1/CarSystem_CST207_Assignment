83dbd48a420b62afb05ad0b7139209574e5edc6b
Crax <55369708+KT-KX@users.noreply.github.com>
2024-06-16 11:52:59 +0800

    Update Main.cpp
    Added quicksort, binary search, linear search, selection sort function with argument

--- Main.cpp
+++ Main.cpp
@@ -70,14 +70,107 @@ class allClient{
                 cout << clients[i].getID() << clients[i].getName() << endl;
             }
         }
-        void sorting(){
+        void selectionSort(int arr[], int n){
+            int i, j, min_idx;
 
+            // One by one move boundary of
+            // unsorted subarray
+            for (i = 0; i < n - 1; i++) {
+
+                // Find the minimum element in
+                // unsorted array
+                min_idx = i;
+                for (j = i + 1; j < n; j++) {
+                    if (arr[j] < arr[min_idx])
+                        min_idx = j;
+                }
+
+                // Swap the found minimum element
+                // with the first element
+                if (min_idx != i)
+                    swap(arr[min_idx], arr[i]);
+            }
         }
-        void binarySearch(){
 
+
+        int partition(int arr[], int start, int end){
+        
+            int pivot = arr[start];
+        
+            int count = 0;
+            for (int i = start + 1; i <= end; i++) {
+                if (arr[i] <= pivot)
+                    count++;
+            }
+        
+            // Giving pivot element its correct position
+            int pivotIndex = start + count;
+            swap(arr[pivotIndex], arr[start]);
+        
+            // Sorting left and right parts of the pivot element
+            int i = start, j = end;
+        
+            while (i < pivotIndex && j > pivotIndex) {
+        
+                while (arr[i] <= pivot) {
+                    i++;
+                }
+        
+                while (arr[j] > pivot) {
+                    j--;
+                }
+        
+                if (i < pivotIndex && j > pivotIndex) {
+                    swap(arr[i++], arr[j--]);
+                }
+            }
+        
+            return pivotIndex;
         }
-        void linearSearch(){
+ 
+        void quickSort(int arr[], int start, int end)
+        {
+        
+            // base case
+            if (start >= end)
+                return;
+        
+            // partitioning the array
+            int p = partition(arr, start, end);
+        
+            // Sorting the left part
+            quickSort(arr, start, p - 1);
+        
+            // Sorting the right part
+            quickSort(arr, p + 1, end);
+        }
+        int binarySearch(int arr[], int low, int high, int x){
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
 
+                // Check if x is present at mid
+                if (arr[mid] == x)
+                    return mid;
+
+                // If x greater, ignore left half
+                if (arr[mid] < x)
+                    low = mid + 1;
+
+                // If x is smaller, ignore right half
+                else
+                    high = mid - 1;
+            }
+
+            // If we reach here, then element was not present
+            return -1;
+        }
+        int LinearSearch(int arr[], int n, int x)
+        {
+            int i;
+            for (i = 0; i < n; i++)
+                if (arr[i] == x)
+                    return i;
+            return -1;
         }
 };
 class allCustomer{
