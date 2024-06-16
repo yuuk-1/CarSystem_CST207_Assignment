698dadfe8dc45784108833dd3d1494f3545c1db6
Crax <55369708+KT-KX@users.noreply.github.com>
2024-06-16 11:56:49 +0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -335,33 +335,121 @@ public:
         // Implement merge sort here
     }
 
-    void quickSort() {
-        copy_cars = cars;
-        // Implement quick sort here
-    }
-
     void radixSort() {
         copy_cars = cars;
         // Implement radix sort here
     }
 
-    void selectionSort() {
-        copy_cars = cars;
-        // Implement selection sort here
-    }
+
 
     void bubbleSort() {
         copy_cars = cars;
         // Implement bubble sort here
     }
 
-    void linearSearch() {
-        // Implement linear search here
-    }
+    void selectionSort(int arr[], int n){
+            copy_cars = cars;
+            int i, j, min_idx;
 
-    void binarySearch() {
-        // Implement binary search here
-    }
+            // One by one move boundary of
+            // unsorted subarray
+            for (i = 0; i < n - 1; i++) {
+
+                // Find the minimum element in
+                // unsorted array
+                min_idx = i;
+                for (j = i + 1; j < n; j++) {
+                    if (arr[j] < arr[min_idx])
+                        min_idx = j;
+                }
+
+                // Swap the found minimum element
+                // with the first element
+                if (min_idx != i)
+                    swap(arr[min_idx], arr[i]);
+            }
+        }
+
+
+        int partition(int arr[], int start, int end){
+        
+            int pivot = arr[start];
+        
+            int count = 0;
+            for (int i = start + 1; i <= end; i++) {
+                if (arr[i] <= pivot)
+                    count++;
+            }
+        
+            // Giving pivot element its correct position
+            int pivotIndex = start + count;
+            swap(arr[pivotIndex], arr[start]);
+        
+            // Sorting left and right parts of the pivot element
+            int i = start, j = end;
+        
+            while (i < pivotIndex && j > pivotIndex) {
+        
+                while (arr[i] <= pivot) {
+                    i++;
+                }
+        
+                while (arr[j] > pivot) {
+                    j--;
+                }
+        
+                if (i < pivotIndex && j > pivotIndex) {
+                    swap(arr[i++], arr[j--]);
+                }
+            }
+        
+            return pivotIndex;
+        }
+ 
+        void quickSort(int arr[], int start, int end){
+            copy_cars = cars;
+        
+            // base case
+            if (start >= end)
+                return;
+        
+            // partitioning the array
+            int p = partition(arr, start, end);
+        
+            // Sorting the left part
+            quickSort(arr, start, p - 1);
+        
+            // Sorting the right part
+            quickSort(arr, p + 1, end);
+        }
+        int binarySearch(int arr[], int low, int high, int x){
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+
+                // Check if x is present at mid
+                if (arr[mid] == x)
+                    return mid;
+
+                // If x greater, ignore left half
+                if (arr[mid] < x)
+                    low = mid + 1;
+
+                // If x is smaller, ignore right half
+                else
+                    high = mid - 1;
+            }
+
+            // If we reach here, then element was not present
+            return -1;
+        }
+        int LinearSearch(int arr[], int n, int x)
+        {
+            int i;
+            for (i = 0; i < n; i++)
+                if (arr[i] == x)
+                    return i;
+            return -1;
+        }
 
     void printCars() const {
         for (const auto &c : cars) {
